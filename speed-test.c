7d531b29877e65f24460e9fb9c062057aadf2438
zxcwn <tires098@gmail.com>
2024-10-09 19:45:28 +0300

    ver 1.0 based; one phrase to check speed

---
+++ speed-test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+// created by zxcwn
+
+#define TEXT "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus."
+#define TEXT_LEN strlen(TEXT)
+#define RESET "\033[0m"
+#define BLACK "\033[1;30m"
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+
+unsigned long long get_current_time_ms() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+int main(){
+	printf("%s%s%s\n", BLACK, TEXT, RESET);
+	system("stty -icanon -echo");
+	unsigned long long current_time_ms = get_current_time_ms();
+	char inp_char;
+	unsigned int success = 0;
+	for (int i = 0; i < TEXT_LEN; ++i) {
+		inp_char = getchar();
+		if (inp_char == TEXT[i]) {
+			printf("%s%c%s", GREEN, inp_char, RESET);
+			success++;
+		}
+		else {printf("%s%c%s", RED, inp_char, RESET);}   	
+	}
+	unsigned long long time_after_sleep = get_current_time_ms();
+	printf("\n%s%d%s/%s%lu\n%s", GREEN, success, BLACK, RED, TEXT_LEN - success, RESET);
+	printf("%sLetters per minute: %f%s\n", BLACK, ((60.0 * success) / ((time_after_sleep - current_time_ms) / 1000.0)), RESET);
+	printf("%sWords per minute: %f%s\n", BLACK, ((60.0 * success / 6.0) / ((time_after_sleep - current_time_ms) / 1000.0)), RESET);
+	system("stty icanon echo");
+}
