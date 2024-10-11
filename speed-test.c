8a6f009524ea2f6d7264334785d291c118c09d4e
zxcwn <tires098@gmail.com>
2024-10-11 18:29:24 +0300

    1.1 added underscores instead of spaces, backspaces now don't mess up

--- speed-test.c
+++ speed-test.c
@@ -12,6 +12,10 @@
 #define RED "\033[1;31m"
 #define GREEN "\033[1;32m"
 
+#define BACKSPACE_ASCII_CODE 127
+#define SPACE_ASCII_CODE 32
+#define SPACE_SYMBOL '_'
+
 unsigned long long get_current_time_ms() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
@@ -26,11 +30,26 @@ int main(){
 	unsigned int success = 0;
 	for (int i = 0; i < TEXT_LEN; ++i) {
 		inp_char = getchar();
-		if (inp_char == TEXT[i]) {
-			printf("%s%c%s", GREEN, inp_char, RESET);
+		if (inp_char == BACKSPACE_ASCII_CODE) {
+			--i;
+		}
+		else if (inp_char == TEXT[i]) {
+			if (inp_char == SPACE_ASCII_CODE) {
+				printf("%s%c%s", GREEN, SPACE_SYMBOL, RESET);
+			}
+			else {
+				printf("%s%c%s", GREEN, inp_char, RESET);
+			}
 			success++;
 		}
-		else {printf("%s%c%s", RED, inp_char, RESET);}   	
+		else {
+			if (inp_char == SPACE_ASCII_CODE) {
+				printf("%s%c%s", RED, SPACE_SYMBOL, RESET);
+			}
+			else {
+				printf("%s%c%s", RED, inp_char, RESET);
+			}
+		}
 	}
 	unsigned long long time_after_sleep = get_current_time_ms();
 	printf("\n%s%d%s/%s%lu\n%s", GREEN, success, BLACK, RED, TEXT_LEN - success, RESET);
