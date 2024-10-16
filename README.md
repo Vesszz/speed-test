eca2386a04be3e3e78775839f61f24d2304c5735
zxcwn <tires098@gmail.com>
2024-10-16 16:18:38 +0300

    2.0 philosophy changed: words now generate randomly

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-## Small project written in 100% C
+# Small project written in 100% C
 ***
+### About
+
+Speed typing test for all keywords in language C
+
 ### Uses:
 > system("stty -icanon -echo");
 
--- speed-test.c
+++ speed-test.c
@@ -5,8 +5,12 @@
 
 // created by zxcwn
 
-#define TEXT "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus."
-#define TEXT_LEN strlen(TEXT)
+char *keywords[32] = {
+    "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch", "typedef", "union", "unsigned", "void", "volatile", "while"
+};
+
+#define TEXT_LEN 1
+#define TEXT "123"
 #define RESET "\033[0m"
 #define BLACK "\033[1;30m"
 #define RED "\033[1;31m"
@@ -15,6 +19,7 @@
 #define BACKSPACE_ASCII_CODE 127
 #define SPACE_ASCII_CODE 32
 #define SPACE_SYMBOL '_'
+#define SPACE ' '
 
 unsigned long long get_current_time_ms() {
     struct timeval tv;
@@ -23,17 +28,28 @@ unsigned long long get_current_time_ms() {
 }
 
 int main(){
-	printf("%s%s%s\n", BLACK, TEXT, RESET);
+	int words_quanitity;
+	printf("%sEnter the quanitity: %s", BLACK, RESET);
+	scanf("%d", &words_quanitity);
+	char string_words[words_quanitity*8];
+	srand(get_current_time_ms());
+	for (int word_number = 0; word_number < words_quanitity; ++word_number) {
+		int random_word_number = abs(rand()) % 32;
+		strcat(string_words, keywords[random_word_number]);
+		strcat(string_words, " ");
+	}
+	string_words[(signed int)strlen(string_words) - 1] = '\0';
+	printf("%s%s%s", BLACK, string_words, RESET);
 	system("stty -icanon -echo");
 	unsigned long long current_time_ms = get_current_time_ms();
 	char inp_char;
 	unsigned int success = 0;
-	for (int i = 0; i < TEXT_LEN; ++i) {
+	for (int i = -1; i < (signed int)strlen(string_words); ++i) {
 		inp_char = getchar();
 		if (inp_char == BACKSPACE_ASCII_CODE) {
 			--i;
 		}
-		else if (inp_char == TEXT[i]) {
+		else if (inp_char == string_words[i]) {
 			if (inp_char == SPACE_ASCII_CODE) {
 				printf("%s%c%s", GREEN, SPACE_SYMBOL, RESET);
 			}
@@ -50,9 +66,9 @@ int main(){
 				printf("%s%c%s", RED, inp_char, RESET);
 			}
 		}
-	}
+    }
 	unsigned long long time_after_sleep = get_current_time_ms();
-	printf("\n%s%d%s/%s%lu\n%s", GREEN, success, BLACK, RED, TEXT_LEN - success, RESET);
+	printf("\n%s%d%s/%s%lu\n%s", GREEN, success, BLACK, RED, (signed int)strlen(string_words) - success, RESET);
 	printf("%sLetters per minute: %f%s\n", BLACK, ((60.0 * success) / ((time_after_sleep - current_time_ms) / 1000.0)), RESET);
 	printf("%sWords per minute: %f%s\n", BLACK, ((60.0 * success / 6.0) / ((time_after_sleep - current_time_ms) / 1000.0)), RESET);
 	system("stty icanon echo");
